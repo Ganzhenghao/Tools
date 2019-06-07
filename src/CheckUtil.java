0c604d1bdd9bd933f4911e6fdaefde978acb3d0d
Ganzhenghao <2454023207@qq.com>
2019-06-07 15:59:00 +0800

    添加字符串是否为空工具类

---
+++ src/CheckUtil.java
@@ -0,0 +1,12 @@
+/**
+ * ClassName CheckUtil
+ * Description TODO
+ * Author Ganzhenghao
+ * Date  2019/6/4 20:49
+ * Version 1.0
+ **/
+public class CheckUtil {
+    public static boolean isEmpty(String s){
+        return s==null||s.length()==0;
+    }
+}
