95dd94980f3bad8cf6f77dfe9de275825b07bf1f
Ganzhenghao <2454023207@qq.com>
2019-06-16 19:22:25 +0800

    添加JDBC_DRUID工具类

--- src/JDBCTools_DRUID.java
+++ src/JDBCTools_DRUID.java
@@ -1,3 +1,12 @@
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.mysql.jdbc.Statement;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
  * ClassName JDBCTools_DRUID
  * Description TODO
@@ -6,4 +15,54 @@
  * Version 1.0
  **/
 public class JDBCTools_DRUID {
+    //定义成员变量
+    private static DataSource dataSource;
+    //静态代码块加载配置
+    static {
+        try {
+            Properties properties = new Properties();
+            //通过ClassLoader加载Src下的文件
+            properties.load(JDBCTools_DRUID.class.getClassLoader().getResourceAsStream("druid.properties"));
+            dataSource  = DruidDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //获取数据库连接池
+    public static DataSource getDataSource(){
+        return dataSource;
+    }
+    //获取连接
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+    //释放资源
+    public static void close(Connection conn, Statement st){
+        close(conn, st, null);
+    }
+    public static void close(Connection conn, Statement st, ResultSet rs) {
+        if (conn!=null){
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (st!=null){
+            try {
+                st.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (rs!=null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
